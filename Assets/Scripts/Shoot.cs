296b135179aa38618ad05ab7ae4b5eebfe1d0ea4
eryckpedro <eryckpedro@yahoo.com.br>
2015-04-01 20:53:17 -0300

    Comandos FPS, atira, som ao atirar
    Quase finalizando o básico pedido

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float force = 50.0f;
+	public float waitTime = 0.4f;
+
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+		time = Time.time;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float deltaTime = Time.time - time;
+
+		if(Input.GetAxis("Fire1") > 0 && deltaTime > waitTime){
+			GameObject bullet = Instantiate(bulletPrefab) as GameObject;
+			bullet.transform.position = this.transform.position;
+			bullet.rigidbody.AddForce(this.transform.forward*force);
+
+			time = Time.time;
+
+			audio.Play();
+		}
+	
+	}
+}
