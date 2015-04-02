3df71e69225477f425780e590831a677a36a4271
eryckpedro <eryckpedro@yahoo.com.br>
2015-04-02 16:53:30 -0300

    Mais refatoração

--- Assets/Scripts/TargetDestruction.cs
+++ Assets/Scripts/TargetDestruction.cs
@@ -5,5 +5,7 @@ public class TargetDestruction : MonoBehaviour {
 
 	void OnCollisionEnter(Collision col)
 	{
+		if(col.gameObject.name == "projectilePrefab(Clone)")
+			Destroy (this.gameObject);
 	}
 }
