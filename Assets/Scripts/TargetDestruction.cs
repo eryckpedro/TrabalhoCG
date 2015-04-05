70fb4b12acbd06ff3a2e0f8081c5982e5def3357
eryckpedro <eryckpedro@yahoo.com.br>
2015-04-05 14:07:28 -0300

    Add Score e ScoreManager
    Sistema de build de pontuação colocada no canto superior direito da tela

--- Assets/Scripts/TargetDestruction.cs
+++ Assets/Scripts/TargetDestruction.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class TargetDestruction : MonoBehaviour {
 
+	public int scoreValue = 10;
+
 	void OnCollisionEnter(Collision col)
 	{
 		if(col.gameObject.name == "projectilePrefab(Clone)")
 			Destroy (this.gameObject);
+
+		ScoreManager.score += scoreValue;
 	}
 }
