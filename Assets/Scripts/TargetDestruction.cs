82b3a0328a1141ca5b316bd1d45d1d44140b101f
eryckpedro <eryckpedro@yahoo.com.br>
2015-04-05 15:36:01 -0300

    Adicionando som ao destruir alvo

--- Assets/Scripts/TargetDestruction.cs
+++ Assets/Scripts/TargetDestruction.cs
@@ -5,11 +5,26 @@ public class TargetDestruction : MonoBehaviour {
 
 	public int scoreValue = 10;
 
+	void DestroyGameObject(){
+		Destroy(this.gameObject);
+	}
+
 	void OnCollisionEnter(Collision col)
 	{
-		if(col.gameObject.name == "projectilePrefab(Clone)")
-			Destroy (this.gameObject);
 
 		ScoreManager.score += scoreValue;
+
+		if(col.gameObject.name == "projectilePrefab(Clone)"){
+			Destroy(this.renderer);
+			Destroy(this.collider);
+
+			audio.Play();
+
+			Invoke("DestroyGameObject", audio.clip.length);
+
+
+		}
+
 	}
+
 }
