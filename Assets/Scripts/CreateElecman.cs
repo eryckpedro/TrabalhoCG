fc63a401576e88ccf4802baa2edf38467f84c259
eryckpedro <eryckpedro@yahoo.com.br>
2015-04-10 16:45:18 -0300

    Elecman Spawner nerfado, melhorias FPS

--- Assets/Scripts/CreateElecman.cs
+++ Assets/Scripts/CreateElecman.cs
@@ -7,50 +7,55 @@ public class CreateElecman : MonoBehaviour {
 	float time = 0f;
 	public float epsilon = 0.5f;
 	public int numberObjects;
-	public int maxNumberObjects = 1;
+	public int maxNumberObjects;
 
 	public float x_min = 1f;
 	public float x_max = 30f;
 	public float z_min = -19f;
-	public float z_max = 30f;
-	public float y = 20f;
+	public float z_max = -8f;
+	public float y_max = 10f;
+	public float y_min = 4f;
 
 	public GameObject ElecmanMovePrefab;
 	public GameObject ElecmanPrefab;
-	public GameObject ElecmanSpherePrefab;
 
-	float x, z;
+	float x, y, z;
+
+	void generateObjects() {
+		time = time + Time.deltaTime;
+		if (time >= timeBetweenCreation) {
+			
+			numberObjects = Random.Range(1, maxNumberObjects);
+			
+			for (int i = 1; i<= numberObjects; i++) {
+				x = Random.Range (x_min, x_max);
+				y = Random.Range (y_min, y_max);
+				z = Random.Range (z_min, z_max);
+				
+				GameObject ElecmanMoveObject = 
+					Instantiate(ElecmanMovePrefab, new Vector3(x,y,z), Quaternion.identity) as GameObject;
+				
+				
+//				x = Random.Range (x_min, x_max);
+//				y = Random.Range (y_min, y_max);
+//				z = Random.Range (z_min, z_max);
+//				GameObject ElecmanObject = 
+//					Instantiate(ElecmanPrefab, new Vector3(x,y,z),  Quaternion.identity) as GameObject;
+				
+			}
+			
+			timeBetweenCreation = Random.Range( timeBetweenCreation - epsilon, timeBetweenCreation + epsilon);
+			time = 0;
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
-	
+		generateObjects();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		time = time + Time.deltaTime;
-		if (time >= timeBetweenCreation) {
-
-			numberObjects = Random.Range(1, maxNumberObjects);
-
-			for (int i = 1; i<= numberObjects; i++) {
-				x = Random.Range (x_min, x_max);
-				z = Random.Range (z_min, z_max);
-
-				GameObject ElecmanMoveObject = Instantiate(ElecmanMovePrefab, new Vector3(x,y,z), Quaternion.identity) as GameObject;
-
-				x = Random.Range (x_min, x_max);
-				z = Random.Range (z_min, z_max);
-				GameObject ElecmanObject = Instantiate(ElecmanPrefab, new Vector3(x,y,z),  Quaternion.identity) as GameObject;
-
-				x = Random.Range (x_min, x_max);
-				z = Random.Range (z_min, z_max);
-				GameObject ElecmanSphereObject = Instantiate(ElecmanSpherePrefab, new Vector3(x,y,z), Quaternion.identity) as GameObject;
-			
-			}
-
-			timeBetweenCreation = Random.Range( timeBetweenCreation - epsilon, timeBetweenCreation + epsilon);
-			time = 0;
-		}
+		generateObjects ();
 	}
 }
