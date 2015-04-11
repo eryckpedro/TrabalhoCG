c41779787d0543f9f7e61fcca42212138a174b97
Hector <hectornmelo@gmail.com>
2015-04-11 11:54:59 -0300

    Criação dos Elecmans e Shadowman / Shadowman se move

--- Assets/Scripts/CreateElecman.cs
+++ Assets/Scripts/CreateElecman.cs
@@ -3,40 +3,112 @@ using System.Collections;
 
 public class CreateElecman : MonoBehaviour {
 
-	public float timeBetweenCreation = 5f;
+	public float timeBetweenCreation = 3f;
 	float time = 0f;
 	public float epsilon = 0.5f;
 	public int numberObjects = 1;
 	public int maxNumberObjects;
 
 	public Vector3 position1 = new Vector3(-15.0f, 9.0f, -15.0f);
-	public Vector3 position2 = new Vector3(-12.0f, 9.0f, -12.0f);
-	public Vector3 position3 = new Vector3(-14.0f, 9.0f, -8.0f);
+	public Vector3 position2 = new Vector3(-12.0f, 9.0f, -10.0f);
+	public Vector3 position3 = new Vector3(-14.0f, 9.0f, -6.0f);
+
+	public Vector3 position4 = new Vector3(-15.0f, 9.0f, 11.0f);
+	public Vector3 position5 = new Vector3(-12.0f, 9.0f, 15.0f);
+	public Vector3 position6 = new Vector3(-14.0f, 9.0f, 19.0f);
+
+	public Vector3 position7 = new Vector3(-15.0f, 12.0f, -15.0f);
+	public Vector3 position8 = new Vector3(-12.0f, 12.0f, -12.0f);
+	public Vector3 position9 = new Vector3(-14.0f, 12.0f, -8.0f);
+
+	public Vector3 position10 = new Vector3(-15.0f, 12.0f, 11.0f);
+	public Vector3 position11 = new Vector3(-12.0f, 12.0f, 15.0f);
+	public Vector3 position12 = new Vector3(-14.0f, 12.0f, 19.0f);
+
+	public Vector3 position13 = new Vector3(-15.0f, 10.0f, 2.0f);
+	public Vector3 position14 = new Vector3(-12.0f, 10.0f, 6.0f);
 
 	
-	public GameObject ElecmanMovePrefab;
-
-	float x, y, z;
+	public GameObject ElecmanMovePrefab, ShadowManPrefab, target;
 
 	void generateObjects() {
 		time = time + Time.deltaTime;
 		if (time >= timeBetweenCreation) {
 
-			int pos = Random.Range(1, 3);
+			int pos = Random.Range(1, 15);
+
+			int targetNumber = Random.Range (1,3);
+
+
+			if (targetNumber == 1) {
+				target = ElecmanMovePrefab;
+			}
+			else {
+				target = ShadowManPrefab;
+			}
 
 			if(pos == 1){
-				GameObject ElecmanMoveObjectPos1 = 
-					Instantiate(ElecmanMovePrefab, position1, Quaternion.identity) as GameObject;
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position1, Quaternion.identity) as GameObject;
 			}
 				
 			else if (pos == 2){
-				GameObject ElecmanMoveObjectPos2 = 
-					Instantiate(ElecmanMovePrefab, position2, Quaternion.identity) as GameObject;
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position2, Quaternion.identity) as GameObject;
 			}
 
 			else if (pos == 3){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position3, Quaternion.identity) as GameObject;
+			}
+			else if (pos == 4){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position4, Quaternion.identity) as GameObject;
+			}
+			
+			else if (pos == 5){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position5, Quaternion.identity) as GameObject;
+			}
+			else if (pos == 6){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position6, Quaternion.identity) as GameObject;
+			}
+			
+			else if (pos == 7){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position7, Quaternion.identity) as GameObject;
+			}
+			else if (pos == 8){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position8, Quaternion.identity) as GameObject;
+			}
+			
+			else if (pos == 9){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position9, Quaternion.identity) as GameObject;
+			}
+			else if (pos == 10){
+				GameObject ElecmanMoveObjectPos = 
+					Instantiate(target, position10, Quaternion.identity) as GameObject;
+			}
+			
+			else if (pos == 11){
 				GameObject ElecmanMoveObjectPos3 = 
-					Instantiate(ElecmanMovePrefab, position3, Quaternion.identity) as GameObject;
+					Instantiate(target, position11, Quaternion.identity) as GameObject;
+			}
+			else if (pos == 12){
+				GameObject ElecmanMoveObjectPos3 = 
+					Instantiate(target, position12, Quaternion.identity) as GameObject;
+			}
+			else if (pos == 13){
+				GameObject ElecmanMoveObjectPos2 = 
+					Instantiate(target, position13, Quaternion.identity) as GameObject;
+			}
+			
+			else if (pos == 14){
+				GameObject ElecmanMoveObjectPos3 = 
+					Instantiate(target, position14, Quaternion.identity) as GameObject;
 			}
 			
 			timeBetweenCreation = Random.Range( timeBetweenCreation - epsilon, timeBetweenCreation + epsilon);
---
+++ Assets/ShadowmanMove.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadowmanMove : MonoBehaviour {
+	
+	// Use this for initialization
+	
+	public float power = 0.000000001f;
+	public int waitTime = 1;
+	float time = 0f;
+	
+	void Start () {
+		this.rigidbody.AddForce (new Vector3(15,0,0));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time = time + Time.deltaTime;
+		if (time > waitTime) {
+			var randomPosition = new Vector3 (Random.Range( -200,200), Random.Range( -200,200), Random.Range( -200,200));
+			this.rigidbody.AddForce(randomPosition, ForceMode.Force);
+			time = 0;
+		}
+	}
+}
