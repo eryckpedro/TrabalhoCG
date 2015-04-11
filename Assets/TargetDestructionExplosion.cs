21ed92930d059f1bed4ad9194170b393d2b32c8a
Hector <hectornmelo@gmail.com>
2015-04-11 12:23:35 -0300

    Criação dos Elecman e Shadowman / Shadowman move

---
+++ Assets/TargetDestructionExplosion.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetDestructionExplosion : MonoBehaviour {
+	
+	public int scoreValue = 10;
+	public Component particleSystemObject;
+	
+	
+	void DestroyGameObject(){
+		Destroy(this.gameObject);
+	}
+	
+	void OnCollisionEnter(Collision col)
+	{	
+		
+		if(col.gameObject.name == "projectilePrefab(Clone)"){
+			
+			this.particleSystem.enableEmission = true;
+			//particleSystem.enableEmission = true;
+			
+			ScoreManager.score += scoreValue;
+			
+			Destroy(this.renderer);
+			Destroy(this.collider);
+			
+			audio.Play();
+			
+			Invoke("DestroyGameObject", audio.clip.length);
+			
+			
+		}
+		
+	}
+	
+}
