58352b4bdad8be1997a26574c7b76129c8b9721b
eryckpedro <eryckpedro@yahoo.com.br>
2015-04-11 15:56:07 -0300

    S1 renomeada, Colocado restart do jogo no GameOver

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -5,18 +5,25 @@ public class GameOverManager : MonoBehaviour {
 	
 	public GameObject bulletPrefab;
 
+	public float restartDelay = 5.0f;
+	private float restartTimer;
+
 	
 	// Use this for initialization
 	void Start () {
-		
+		restartTimer = 0.0f;
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		
-		if(Input.GetAxis("Fire1") > 0){
-			Destroy(this.gameObject);
+
+		restartTimer += Time.deltaTime;
+
+		if(restartTimer >= restartDelay){
+
+			Application.LoadLevel("Main");
 		}
-		
+
 	}
 }
